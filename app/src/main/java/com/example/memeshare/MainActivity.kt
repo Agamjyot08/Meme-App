3ee9f89ea933aab9d673008cda9b71a11d579c48
Agamjyot08 <singhagamjyot@gmail.com>
2021-09-08 15:50:06 +0530

    Initial Commit

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.memeshare
 
+import android.app.DownloadManager
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -11,39 +16,88 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
+
+    var memeUrl: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMeme()
     }
 
-    private fun loadMeme() {
+    //    function to load a meme
+    fun loadMeme() {
+
+        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
+        progressBar.visibility = View.VISIBLE;
+
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
-
-// Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET, url, null,
-            Response.Listener { response ->
-                val url = response.getString("url")
-                val memeImage = findViewById<ImageView>(R.id.memeImage)
-                Glide.with(this).load(url).into(memeImage)
+            Request.Method.GET,url,null,
+            Response.Listener {
+                memeUrl = it.getString("url")
+                val memeImage : ImageView = findViewById(R.id.memeImage)
+
+                Glide.with(this).load(memeUrl).listener(object : RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                     progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                }).into(memeImage)
             },
             Response.ErrorListener {
-                Toast.makeText(this, "Something went wrong!", Toast.LENGTH_LONG).show()
-            })
 
-// Add the request to the RequestQueue.
+            }
+        )
         queue.add(jsonObjectRequest)
     }
 
     fun shareMeme(view: View) {
-
-
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra("Check this meme",memeUrl)
+        val chooser = Intent.createChooser(intent,"Share meme using....")
+        startActivity(chooser)
     }
+
     fun nextMeme(view: View) {
-
-
+        loadMeme()
     }
+
+    fun downloadMeme(view: View) {
+        val request = DownloadManager.Request(Uri.parse(memeUrl))
+            .setTitle("Meme")
+            .setDescription("Downloading")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setAllowedOverMetered(true)
+
+        val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        dm.enqueue(request)
+    }
+
 }
